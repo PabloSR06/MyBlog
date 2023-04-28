3d620c7350d630973ae054ec7f28f2e455c798d8
Pablo <47283527+PabloSR06@users.noreply.github.com>
2023-04-28 20:32:43 +0100

    FEAT: List Posts

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace MyBlog.Models
+{
+    public class Post
+    {
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+        [JsonProperty("fileName")]
+        public string? fileName { get; set; }
+        [JsonProperty("date")]
+        public string? Date { get; set; }
+
+    }
+}
