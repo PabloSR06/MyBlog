7426767fe426e8d8d380181e8c3f5e3e63dadd22
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2024-01-20 15:10:13 +0000

    Revert "feat/externalpost"

--- Models/Post.cs
+++ Models/Post.cs
@@ -7,17 +7,13 @@ namespace MyBlog.Models
         [JsonProperty("title")]
         public string? Title { get; set; }
         [JsonProperty("fileName")]
-        public string fileName { get; set; }
+        public string? fileName { get; set; }
         [JsonProperty("date")]
         public string? Date { get; set; }
         [JsonProperty("tags")]
         public List<string>? tags { get; set; }
         [JsonProperty("description")]
         public string? description { get; set; }
-        [JsonProperty("isExternal")]
-        public bool isExternal { get; set; }
-        [JsonProperty("url")]
-        public Uri? url { get; set; }
 
     }
 }
