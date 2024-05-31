84318f9a53f7029d786ef9a5625cb27e89345ec4
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2024-05-31 23:16:52 +0100

    feat: aria label

---
+++ src/data/texts.ts
@@ -0,0 +1,5 @@
+export const AltLinkedin = "Linkedin link for Pablo Suarez";
+export const AltTwitter = "Twitter link for Pablo Suarez";
+export const AltGithub = "Github link for Pablo Suarez";
+export const AltBuyMeACoffe = "Buy Me A Coffe link for Pablo Suarez";
+export const AltHome = "Go to Home Page";
