d4a4c4a0a9714bce3b57ae6582f0abd71e988684
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2024-05-31 23:46:06 +0100

    fix: meta property

--- src/components/postInfo.tsx
+++ src/components/postInfo.tsx
@@ -19,11 +19,15 @@ export const PostInfo:React.FC<PostInfoProps> = ({post}) => {
         <div className={styles.post}>
             <Helmet>
                 <title>{post.postInfo.title}</title>
-                <meta name="description" content={post.postInfo.description} />
-                <meta name="theme-color" content="#008f68" />
+                <meta name="og:description" content={post.postInfo.description}/>
+                <meta name="theme-color" content="#008f68"/>
+                <meta name="og:url" content={`https://blog.pablosr.com/post/${post.postInfo.id}`}/>
+                <meta property="og:type" content="article"/>
+                <meta name="keywords" content={post.postInfo.tags.join(", ")}/>
+
             </Helmet>
             <div className={styles.postInfo}>
-                <h2 className={styles.postTitle}>{post.postInfo.title}</h2>
+            <h2 className={styles.postTitle}>{post.postInfo.title}</h2>
                 <p className={styles.postDate}>{post.postInfo.date}</p>
             </div>
             <div>
